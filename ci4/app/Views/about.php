b69903829f686f2d8c2f955357f6e95b0c088759
Roby Sunjaya <robysunjaya9@gmail.com>
2025-03-22 00:32:39 +0700

    Deskripsi perubahan

---
+++ ci4/app/Views/about.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html> 
+<html lang="en"> 
+<head> 
+    <meta charset="UTF-8"> 
+    <title><?= $title; ?></title>
+    <link rel="stylesheet" href="style.css"> 
+</head> 
+<body>
+    <?= $this->include('template/header'); ?> 
+ 
+    <h1><?= $title; ?></h1> 
+    <hr> 
+    <p><?= $content; ?></p> 
+  
+ <?= $this->include('template/footer'); ?>
+</body> 
+</html> 
